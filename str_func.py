3ccb9f19ee258910b44f0ede71a49b6db46dc323
Your Name <you@example.com>
2023-07-13 01:56:32 +0600

    funct

--- str_func.py
+++ str_func.py
@@ -1,6 +1,14 @@
+"""let there be upper letters"""
 def upper_symb():
     symb = input('Введите набор символов: ').upper()
     return symb
 
-
 print(upper_symb())
+
+
+
+"""I want more capitalized letters"""
+def Capitalize(S):
+   return S.title()
+S = input()
+print (Capitalize(S))
